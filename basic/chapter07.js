d50f708c67883112a80d9e9306abd0c2ec35eb75
bluefox182 <fergaldoss@gmail.com>
2021-04-22 00:09:36 -0500

    Understanding Case Sensitivity in Variables

---
+++ basic/chapter07.js
@@ -0,0 +1,9 @@
+// Understanding Case Sensitivity in Variables
+
+/*Best Practice
+Write variable names in JavaScript in camelCase. 
+In camelCase, multi-word variable names have the first word in lowercase and 
+the first letter of each subsequent word is capitalized.*/
+var someVariable;
+var anotherVariableName;
+var thisVariableNameIsSoLong;
