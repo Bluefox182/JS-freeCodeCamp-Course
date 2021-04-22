3ceba96bf360781dbf1f82f7308f364e89b3ee77
bluefox182 <fergaldoss@gmail.com>
2021-04-22 11:05:28 -0500

    Compound Assignment With Augmented Division

---
+++ basic/chapter21.js
@@ -0,0 +1,16 @@
+// Compound Assignment With Augmented Division
+
+// The /= operator divides a variable by another number.
+myVar = myVar / 5;
+// Will divide myVar by 5. This can be rewritten as:
+myVar /= 5;
+
+// Examples
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Testing
+a /= 12;
+b /= 4;
+c /= 11;
