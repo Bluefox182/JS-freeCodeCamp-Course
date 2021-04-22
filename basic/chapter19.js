4bb94bf27a149d2c2d04ad0ebd099525d2e682b5
bluefox182 <fergaldoss@gmail.com>
2021-04-22 00:59:01 -0500

    Compound Assignment With Augmented Subtraction

---
+++ basic/chapter19.js
@@ -0,0 +1,15 @@
+// Compound Assignment With Augmented Subtraction
+
+// Like the += operator, -= subtracts a number from a variable.
+myVar = myVar - 5;
+// will subtract 5 from myVar. This can be rewritten as:
+myVar -= 5;
+// Example
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Results
+a -= 6;
+b -= 15;
+c -= 1;
