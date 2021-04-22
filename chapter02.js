e4d0f1a34b4d73a1c958a797f2ed35dae25dd10d
bluefox182 <fergaldoss@gmail.com>
2021-04-21 23:43:37 -0500

    creating a variable

---
+++ chapter02.js
@@ -0,0 +1,5 @@
+/* n JavaScript we end statements with semicolons. 
+Variable names can be made up of numbers, letters, and $ or _, 
+but may not contain spaces or start with a number. */
+
+var myName;
