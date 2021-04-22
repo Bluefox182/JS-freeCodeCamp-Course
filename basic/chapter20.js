d22a7f07320daaad4919f87dbb8153ca87115419
bluefox182 <fergaldoss@gmail.com>
2021-04-22 01:00:36 -0500

    Compound Assignment With Augmented Multiplication

---
+++ basic/chapter20.js
@@ -0,0 +1,15 @@
+// Compound Assignment With Augmented Multiplication
+
+// The *= operator multiplies a variable by a number.
+myVar = myVar * 5;
+// will multiply myVar by 5. This can be rewritten as:
+myVar *= 5;
+// Example
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Testing
+a *= 5;
+b *= 3;
+c *= 10;
