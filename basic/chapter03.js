a050fa386fdb9f1a9fc89315c39804c8487712f8
bluefox182 <fergaldoss@gmail.com>
2021-04-21 23:48:15 -0500

    values to a var

---
+++ basic/chapter03.js
@@ -0,0 +1,7 @@
+//In JavaScript, you can store a value in a variable with the assignment operator (=).
+var myVar;
+
+/*If there are any calculations to the right of the = operator, 
+those are performed before the value is assigned to the variable 
+on the left of the operator. */
+myVar = 7;
