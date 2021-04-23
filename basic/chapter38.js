f215a88680b9d1614bce1e0c4f6b7c06e10e24a6
bluefox182 <fergaldoss@gmail.com>
2021-04-23 00:05:38 -0500

    Nest one Array within Another Array

---
+++ basic/chapter38.js
@@ -0,0 +1,13 @@
+// Nest one Array within Another Array
+
+// You can also nest arrays within other arrays, like below:
+[
+  ["Bulls", 23],
+  ["White Sox", 45],
+];
+
+// Example #2
+var myArray = [
+  ["My Age is", 36],
+  ["And he is", 27],
+];
