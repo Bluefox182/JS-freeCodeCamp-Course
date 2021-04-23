bb2157e4b5884a6ab348d34cdd44356434c323c3
bluefox182 <fergaldoss@gmail.com>
2021-04-22 23:06:42 -0500

    Use Bracket Notation to Find the Nth Character in a String

---
+++ basic/chapter33.js
@@ -0,0 +1,18 @@
+// Use Bracket Notation to Find the Nth Character in a String
+
+/*
+You can also use bracket notation to get the character at other 
+positions within a string.
+
+Remember that computers start counting at 0, so the first character 
+is actually the zeroth character. */
+
+// Example
+
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+// secondLetterOfFirstName would have a value of the string d.
+
+// Example #2
+var lastName = "Lovelace";
+var thirdLetterOfLastName = lastName[2]; // Third letter is v
