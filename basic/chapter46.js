20cb3dd0e2c56a0e68173972c991db945f47c698
bluefox182 <fergaldoss@gmail.com>
2021-04-23 12:08:04 -0500

    Shopping List

---
+++ basic/chapter46.js
@@ -0,0 +1,20 @@
+// Shopping List
+
+/*
+Create a shopping list in the variable myList. 
+The list should be a multi-dimensional array containing several sub-arrays.
+
+The first element in each sub-array should contain a string with 
+the name of the item. The second element should be a number 
+representing the quantity i.e. */
+
+["Chocolate Bar", 15];
+
+// Example - Create a Shopping List
+var myList = [
+  ["Cookies", 12],
+  ["Milk", 15],
+  ["Chocolate", 12],
+  ["Soap", 5],
+  ["Rice", 10],
+];
