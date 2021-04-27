d11cd7ba19e2e20521286b8132001586951a597c
bluefox182 <fergaldoss@gmail.com>
2021-04-26 22:47:47 -0500

    Return Early Pattern for Functions

---
+++ basic/chapter78.js
@@ -0,0 +1,30 @@
+// Return Early Pattern for Functions
+
+/* When a return statement is reached, the execution of the current function 
+stops and control returns to the calling location. */
+
+// Example
+function myFun() {
+  console.log("Hello");
+  return "World";
+  console.log("byebye");
+}
+myFun();
+/* The above will display the string Hello in the console, and return the string World. 
+The string byebye will never display in the console, because the function exits at the
+return statement. */
+
+// Exercise
+/* Modify the function abTest so that if a or b are less than 0 the function will 
+immediately exit with a value of undefined. */
+// Setup
+function abTest(a, b) {
+  if (a < 0 || b < 0) {
+    // This is
+    return undefined; // the Answer
+  }
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2, 2);
